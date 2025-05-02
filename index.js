fc9d0f7bed35cc0dbcc092d344bff33e5b704ac7
kssudha-techrayslabs <ksaisudha2002@gmail.com>
2025-05-02 21:02:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require("express")
+const http=require("http")
+const app=express()
+const server=http.createServer(app)
+const path=require('path')
+const {Server}=require("socket.io")
+const io=new Server(server)
+
+//io connection
+io.on("connection",(socket)=>{
+    socket.on("user-message",(message)=>{
+        io.emit("message",message)
+    });
+});
+
+
+app.use(express.static('public'));
+
+app.use(express.static(path.resolve('./public')))
+
+app.get('/',(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
+
+
+
+server.listen(9000,()=>console.log(`Server started at port:9000`))
